63080b23e4db601d3b78dc79479a6bf5bb6ee2ab
DustinMC1802 <dustinmcollier@gmail.com>
2022-02-26 09:11:53 -0800

    Method skeletons added to FoodTruck Class for id number, name, type, and rating

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -2,4 +2,27 @@ package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
 
+	
+//	Create methods to assign fields to id number, Name, food type, and rating
+	
+	private int idNumber() {
+		int idNum = 0;
+		return idNum;
+	}
+	
+	private String truckName() {
+		String name = "";
+		return name;
+		
+	}
+	
+	private String foodType() {
+		String type = "";
+		return type;
+	}
+	
+	private int rating () {
+		int rating = 0;
+		return rating;
+	}
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -3,8 +3,8 @@ package com.skilldistillery.foodtruck;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
