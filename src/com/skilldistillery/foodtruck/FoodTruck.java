d4e0491e2d30334fab94e67e0e7dfc6625a0f835
DustinMC1802 <dustinmcollier@gmail.com>
2022-02-26 09:27:01 -0800

    Methods foodType and truckRating updated

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -13,22 +13,22 @@ public class FoodTruck {
 		int idNum = 0;
 		return idNum;
 	}
-	
 	private String truckName() {
 		String name = "";
 		System.out.println("Please enter the Food Truck's name:");
 		name = input.next();
 		return name;
-		
 	}
-	
 	private String foodType() {
 		String type = "";
+		System.out.println("Please enter the Food Truck's food type:");
+		type = input.next();
 		return type;
 	}
-	
 	private int rating () {
 		int rating = 0;
+		System.out.println("Please enter your rating for the Food Truck:");
+		rating = input.nextInt();
 		return rating;
 	}
 }
