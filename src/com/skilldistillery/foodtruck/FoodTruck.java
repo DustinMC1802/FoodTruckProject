cd3d6724d66d342b0af97c84687c111dae993b8e
DustinMC1802 <dustinmcollier@gmail.com>
2022-02-26 09:22:02 -0800

    Scanner added to FoodTruck Class and truckName Method written

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,6 +1,10 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
 public class FoodTruck {
+	
+	Scanner input = new Scanner(System.in);
 
 	
 //	Create methods to assign fields to id number, Name, food type, and rating
@@ -12,6 +16,8 @@ public class FoodTruck {
 	
 	private String truckName() {
 		String name = "";
+		System.out.println("Please enter the Food Truck's name:");
+		name = input.next();
 		return name;
 		
 	}
