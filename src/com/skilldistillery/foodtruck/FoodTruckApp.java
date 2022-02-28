dc03ec91896ffb64e7ce7a567f8483e3a772467d
DustinMC1802 <dustinmcollier@gmail.com>
2022-02-27 23:34:16 -0800

    Menu print-out created

--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -33,6 +33,14 @@ public class FoodTruckApp {
 		}
 
 		System.out.println("");
+		
+		System.out.println("Please choose from the following:");
+		System.out.println("Press 1 to list all existing food trucks");
+		System.out.println("Press 2 to see the average rating of the food trucks");
+		System.out.println("Press 3 to display the highest rated food truck");
+		System.out.println("Press 4 to quit");
+		
+		
 
 		sc.close();
 	}
